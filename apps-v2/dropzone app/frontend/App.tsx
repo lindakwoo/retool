52453abbb0febe2f9e9e8309f42ff50882bee83d
Linda Woo <lindakwoo@retool.com>
2026-06-24 17:20:09 +0000

    Update app from agent edits
    Write the latest thread branch snapshot to source control.

--- apps-v2/dropzone app/frontend/App.tsx
+++ apps-v2/dropzone app/frontend/App.tsx
@@ -1,24 +1,5 @@
-/** @jsxRuntime automatic */
-/* eslint-disable react-perf/jsx-no-new-object-as-prop */
-
-import { StarterCanvas } from './components/StarterCanvas'
+import FileUploadPage from './pages/FileUploadPage'
 
 export default function App() {
-  return (
-    <div
-      style={{
-        fontFamily: 'sans-serif',
-        padding: 0,
-        margin: 0,
-        background: '#F0EFEE', // StarterCanvas u_gray
-        position: 'relative',
-        width: '100vw',
-        height: '100vh',
-        overflow: 'hidden',
-      }}
-    >
-      <StarterCanvas fadeIn={false} />
-    </div>
-  )
+  return <FileUploadPage />
 }
-/* eslint-enable react-perf/jsx-no-new-object-as-prop */
