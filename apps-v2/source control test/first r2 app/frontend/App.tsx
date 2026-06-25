f827366cd73159abfaaa2dc66e8518e2fa993cf9
Linda Woo <59804818+lindakwoo@users.noreply.github.com>
2026-06-25 11:07:08 -0700

    Delete apps-v2/source control test/first r2 app directory

--- apps-v2/source control test/first r2 app/frontend/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-green-950 dark:bg-green-950">
-      <h1 className="text-4xl font-bold text-green-300 dark:text-green-200">hello Debbie</h1>
-    </div>
-  )
-}
