12f0eda10a8877b8d6ff926da6b60b66d5aea9c5
Linda Woo <59804818+lindakwoo@users.noreply.github.com>
2026-08-26 09:07:23 -0700

    Add greeting 'hello Woo!' to App component

--- apps-v2/Woo_published/hello linda/frontend/App.tsx
+++ apps-v2/Woo_published/hello linda/frontend/App.tsx
@@ -18,6 +18,7 @@ export default function App() {
       }}
     >
       <h1 className="text-6xl font-bold p-6">hello linda!</h1>
+      <h1 className="text-6xl font-bold p-6">hello Woo!</h1>
       <StarterCanvas fadeIn={false} />
     </div>
   )
